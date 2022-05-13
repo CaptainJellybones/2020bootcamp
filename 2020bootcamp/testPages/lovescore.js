7a3eab9ae7b721de87db4c54f6761e662283afa7
Wejde Mattias <44872222+CaptainJellybones@users.noreply.github.com>
2022-05-13 17:46:49 +0200

    Folder location change
    Literally no other change. I can't believe this created a commit.

---
+++ 2020bootcamp/testPages/lovescore.js
@@ -0,0 +1,10 @@
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if (loveScore >= 90) {
+    console.log("You got " + loveScore + ". Gadamn");
+} else {
+    console.log("Your love score is: " + loveScore);
+}
+
+
