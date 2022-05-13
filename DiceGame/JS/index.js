7c36367e640d08fe03163b9e7dfbd3e2beaa1f3b
CaptainJellybones <44872222+CaptainJellybones@users.noreply.github.com>
2022-05-13 17:19:16 +0200

    Dice game finished

--- DiceGame/JS/index.js
+++ DiceGame/JS/index.js
@@ -1,3 +1,22 @@
 let player1Dice = document.querySelector("body > div > div:nth-child(2) > img");
 let player2Dice = document.querySelector("body > div > div:nth-child(3) > img");
 
+const dice = ["./IMG/dice1.png", "./IMG/dice2.png", "./IMG/dice3.png", "./IMG/dice4.png", "./IMG/dice5.png", "./IMG/dice6.png"]
+
+function getRandomDice() {
+    let p1Dice = Math.floor(Math.random() * dice.length);
+    let p2Dice = Math.floor(Math.random() * dice.length);
+
+    player1Dice.setAttribute("src", dice[p1Dice]);
+    player2Dice.setAttribute("src", dice[p2Dice]);
+
+    if (p1Dice == p2Dice) {
+        document.querySelector("body > div > h1").textContent = "Draw!"
+    } else if (p1Dice > p2Dice) {
+        document.querySelector("body > div > h1").textContent = "🚩 Player 1 Wins!"
+    } else {
+        document.querySelector("body > div > h1").textContent = "Player 2 Wins! 🚩"
+    }
+}
+
+getRandomDice();
