a61bccec71c5881ae12135f69e86ba6d8d0da094
CaptainJellybones <44872222+CaptainJellybones@users.noreply.github.com>
2022-05-17 12:05:43 +0200

    I gotta learn to comment more

--- DiceGame/JS/index.js
+++ DiceGame/JS/index.js
@@ -1,8 +1,11 @@
+// take the element selector of all dice elements
 let player1Dice = document.querySelector("body > div > div:nth-child(2) > img");
 let player2Dice = document.querySelector("body > div > div:nth-child(3) > img");
 
+// Array with all the images src
 const dice = ["./IMG/dice1.png", "./IMG/dice2.png", "./IMG/dice3.png", "./IMG/dice4.png", "./IMG/dice5.png", "./IMG/dice6.png"]
 
+// Pick out one random src, update each dice and declare a winner
 function getRandomDice() {
     let p1Dice = Math.floor(Math.random() * dice.length);
     let p2Dice = Math.floor(Math.random() * dice.length);
