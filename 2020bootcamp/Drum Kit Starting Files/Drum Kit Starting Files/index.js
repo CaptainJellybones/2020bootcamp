a53edf61aebc2721e5f1ce7a3706b27275c16685
Wejde Mattias <44872222+CaptainJellybones@users.noreply.github.com>
2022-06-13 20:32:58 +0200

    Repaired files lost on main pc

--- 2020bootcamp/Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ 2020bootcamp/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,5 +1,10 @@
-document.querySelector("button").addEventListener("click", handleClick);
+const numberOfButtons = document.querySelectorAll('.drum');
 
 function handleClick() {
     alert("I got clicked!");
 }
+
+// ForEach button in numberOfButtons, add an eventlistener to the current element that listens for click and fires handleClick when click is true
+numberOfButtons.forEach(element => {
+    element.addEventListener("click", handleClick);
+});
