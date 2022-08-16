ee4cee60677c836c29ff8b73c8f5aa572c96e950
CaptainJellybones <44872222+CaptainJellybones@users.noreply.github.com>
2022-08-16 17:18:48 +0200

    Finished all of the drum sounds
    Perhaps a bit prematurely

--- 2020bootcamp/Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ 2020bootcamp/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,10 +1,60 @@
 const numberOfButtons = document.querySelectorAll('.drum');
+const audio = ['./sounds/crash.mp3', './sounds/kick-bass.mp3', './sounds/snare.mp3', './sounds/tom-1.mp3', './sounds/tom-2.mp3', './sounds/tom-3.mp3', './sounds/tom-4.mp3']
 
 function handleClick() {
-    alert("I got clicked!");
+    this.style.color = "white";
+    switch (this.innerHTML) {
+        case 'w':
+            sound = new Audio(audio[0])
+            sound.load();
+            sound.play();
+            break;
+
+        case 'a':
+            sound = new Audio(audio[1])
+            sound.load();
+            sound.play();
+            break;
+
+        case 's':
+            sound = new Audio(audio[2])
+            sound.load();
+            sound.play();
+            break;
+
+        case 'd':
+            sound = new Audio(audio[3])
+            sound.load();
+            sound.play();
+            break;
+
+        case 'j':
+            sound = new Audio(audio[4])
+            sound.load();
+            sound.play();
+            break;
+
+        case 'k':
+            sound = new Audio(audio[5])
+            sound.load();
+            sound.play();
+            break;
+
+        case 'l':
+            sound = new Audio(audio[6])
+            sound.load();
+            sound.play();
+            break;
+
+        default:
+            console.log(this.innerHTML + "Does not exist");
+            break;
+    }
+    // this.style.color = "#DA0463";
 }
 
 // ForEach button in numberOfButtons, add an eventlistener to the current element that listens for click and fires handleClick when click is true
 numberOfButtons.forEach(element => {
-    element.addEventListener("click", handleClick);
+    element.addEventListener("click", handleClick)
 });
+
