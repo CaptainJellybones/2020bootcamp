795270cd10f84d6cec742ab6086fb48639df022d
Wejde Mattias <44872222+CaptainJellybones@users.noreply.github.com>
2023-07-05 14:28:46 +0200

    Adding jQuery

--- 2020bootcamp/jQuery/index.js
+++ 2020bootcamp/jQuery/index.js
@@ -1,2 +1,21 @@
 $(document.re)
+//below is bad practice, generally
 $("h1").css('color', 'red');
+
+// add CSS Classes
+$("button").addClass("big");
+
+//does this have class? 
+console.log($("h1").hasClass("peepeepoopoo"));
+
+/*
+.text can be used to change the text of an element
+.html can be used to change the HTML of an element
+*/ 
+
+$("h1").text("Bruh watch your jet!");
+$("button").html("<em>Hey</em>");
+
+
+
+console.log($("img").attr("src"));
